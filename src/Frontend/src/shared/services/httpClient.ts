2696c174f1af6791885d12f8273370d495be89b2
Tomasz Maciążek <tommac.opole@gmail.com>
2025-12-14 14:34:28 +0100

    dedicated config file to read enviromental variables for frontend application

--- src/Frontend/src/shared/services/httpClient.ts
+++ src/Frontend/src/shared/services/httpClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { appConfig } from '../config/appConfig';
 
 export const httpClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: appConfig.apiUrl,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
